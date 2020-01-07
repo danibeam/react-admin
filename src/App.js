27d0b06d54ea4f891c2cc6d7ec7b198c28a299bd
Daniel Belmonte <d.belmonte.amoros@accenture.com>
2020-01-07 18:00:14 +0100

    Full test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Tabletop from 'tabletop';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Tabletop.init( {
+      key: '1JTtwmbmQpv20DCd5KjmTNAGOFuf_dariAeBxbqE-FcY',
+      simpleSheet: true }
+    ).then(function(result, tabletop) {
+      setData(result);
+    }).catch(error => {
+      console.error('there was an error retrieving the data... ', error);
+    })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>React Admin</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Powered by Google Sheets <br />
+          See post followed on <a href="https://medium.com/@ryan.mcnierney/using-react-google-sheets-as-your-cms-294c02561d59" target="blank" rel="noopener noreferrer">Medium</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section>
+        <h2>Here's your data!</h2>
+          <table>
+            <thead>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+            </thead>
+            {
+              data.map(field => {
+                return (
+                    <tbody key={field.Id}>
+                      <td>{field.Id}</td>
+                      <td>{field.Name}</td>
+                      <td>{field.Email}</td>
+                    </tbody>
+                )
+              })
+            }
+        </table>
+      </section>
+      <footer className="App-footer">
+        <a href="https://dbelmonte.me" className="App-link" target="blank" rel="noopener noreferrer">
+          Daniel Belmonte
+        </a> | GNU General Public License v3.0 
+      </footer>
     </div>
   );
 }
